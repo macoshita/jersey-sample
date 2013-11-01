71d09cd0513ac9ae2015dc649642c3c5727e9a52
mirutover <mirutover@gmail.com>
2013-11-01 16:21:28 +0900

    パッケージ名を変更

--- src/main/java/com/github/mirutover/jerseysample/MyApplication.java
+++ src/main/java/com/github/mirutover/jerseysample/MyApplication.java
@@ -1,4 +1,4 @@
-package com.bugsrock.jerseysample;
+package com.github.mirutover.jerseysample;
 
 import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -7,7 +7,9 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class MyApplication extends ResourceConfig {
 
     public MyApplication() {
-        packages("com.bugsrock.jerseysample.controllers");
+        packages("com.github.mirutover.jerseysample.controllers");
+        
+        register(org.glassfish.jersey.server.mvc.MvcFeature.class);
     }
 
 }
